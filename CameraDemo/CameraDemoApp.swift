904b9ebfe6fb3d8fe382a3de6699e84589d9c5ca
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-03-29 05:32:36 -0400

    Initial Commit

---
+++ CameraDemo/CameraDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraDemoApp.swift
+//  CameraDemo
+//
+//  Created by Lori Rothermel on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct CameraDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
