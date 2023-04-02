2ea7e24ac761248e0b65e07d4b219b6067aceb69
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-04-01 22:02:21 -0400

    Camera works. Saves the photo to the library.

--- CameraDemo/ContentView.swift
+++ CameraDemo/ContentView.swift
@@ -11,38 +11,55 @@ struct ContentView: View {
     
     @State private var isShowPhotoLibrary = false
     @State private var image = UIImage()
+       
     
     var body: some View {
+        
+            
         VStack {
+            
+            Spacer()
             
             Image(uiImage: self.image)
                 .resizable()
                 .scaledToFill()
-                .frame(minWidth: 0, maxWidth: .infinity)
-                .edgesIgnoringSafeArea(.all)
+                .frame(width: 350, height: 350)
+                .contextMenu{
+                    Button {
+                        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+                       
+                    } label: {
+                        Text("Save this picture to your Library?")
+                        Image(systemName: "square.and.arrow.down.fill")
+                    }
+                }  // .contentMenu
+                    
+            Spacer()
             
-            Button(action: {
+            Button {
                 self.isShowPhotoLibrary = true
-            }) {
+            } label: {
                 HStack {
                     Image(systemName: "camera")
                         .font(.system(size: 20))
-                        
                     Text("Camera")
                         .font(.headline)
-                }
+                }  // HStack
                 .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 50)
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .cornerRadius(20)
-                .padding(.horizontal)
-            }
-        }
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(20)
+                    .padding(.horizontal)
+            }  // Button
+            
+        }  // VStack
         .sheet(isPresented: $isShowPhotoLibrary) {
             ImagePicker(selectedImage: self.$image, sourceType: .camera)
-        }
-    }
-}
+        }  // .sheet
+     
+        
+    }  // some View
+}  // ContentView
 
 
 
--- CameraDemo/ImagePicker.swift
+++ CameraDemo/ImagePicker.swift
@@ -10,32 +10,32 @@ import SwiftUI
 
 
 struct ImagePicker: UIViewControllerRepresentable {
-        
+    
     @Binding var selectedImage: UIImage
     @Environment(\.presentationMode) private var presentationMode
- 
+    
     var sourceType: UIImagePickerController.SourceType = .photoLibrary
     
- 
+    
     final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-     
+        
         var parent: ImagePicker
-     
+        
         init(_ parent: ImagePicker) {
             self.parent = parent
         }  // init
-     
+        
         func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-     
+            
             if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
                 parent.selectedImage = image
             }  // if let image
-     
+            
             parent.presentationMode.wrappedValue.dismiss()
         }  // func imagePickerController
     }  // class Coordinator
     
-
+    
     func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
         
         let imagePicker = UIImagePickerController()
@@ -55,3 +55,4 @@ struct ImagePicker: UIViewControllerRepresentable {
     }  // func makeCoordinator
     
 }
+
